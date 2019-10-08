7a16f7b7dccc17cc9d97e1877bda6255c3c3c443
Jandersolutions <jba1719@gmail.com>
2019-10-08 13:34:59 -0300

    Update install_nominatim_osrm.sh

--- install_nominatim_osrm.sh
+++ install_nominatim_osrm.sh
@@ -13,8 +13,7 @@ mkdir nominatimdata
 cd nominatimdata
 wget -c http://download.geofabrik.de/south-america/brazil-latest.osm.pbf
 cd
-# sudo docker run -t -v /home/passdriver/nominatimdata:data mediagis/nominatim sh /app/init.sh /data/brazil-latest.osm.pbf postgresdata 4
-# sudo docker run --restart=always -p 6432:5432 -p 7070:8080 -d --name mediagis/nominatim -v /home/passdriver/nominatimdata/postgresdata:/var/lib/postgresql/11/main nominatim bash /app/start.sh
+docker run -t -v "${PWD}:/data" mediagis/nominatim  sh /app/init.sh /data/brazil-latest.osm.pbf postgresdata 4# sudo docker run --restart=always -p 6432:5432 -p 7070:8080 -d --name mediagis/nominatim -v /home/passdriver/nominatimdata/postgresdata:/var/lib/postgresql/11/main nominatim bash /app/start.sh
 # sudo docker run --restart=always -p 6432:5432 -d --name mediagis/nominatim -v /home/passdriver/nominatimdata/postgresdata:/var/lib/postgresql/11/main nominatim sh /app/startpostgres.sh
 # local.php -> @define('CONST_Database_DSN', 'pgsql://nominatim:password1234@192.168.1.128:6432/nominatim'); // <driver>://<username>:<password>@<host>:<port>/<database>
 # sudo docker run --restart=always -p 7070:8080 -d -v /home/passdriver/nominatimdata/conf:/data nominatim sh /app/startapache.sh
